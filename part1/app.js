a40f11d500636b825f1defce3aa3694ffc2d6d42
james <james.lee@student.adelaide.edu.au>
2025-06-20 11:14:47 +0000

    [automatic commit] 2025-06-20 11:14:47 UTC

--- part1/app.js
+++ part1/app.js
@@ -47,13 +47,12 @@ let db;
     `);
 
     await db.execute(`
-      CREATE TABLE Users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      CREATE TABLE Dogs (
+        dog_id INT AUTO_INCREMENT PRIMARY KEY,
+        owner_id INT NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        size ENUM('small', 'medium', 'large') NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id)
     )
     `);
 
