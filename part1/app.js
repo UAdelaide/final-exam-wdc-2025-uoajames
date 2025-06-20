159e92bc993fb1e8df0150b0f74c586c89dad03e
james <james.lee@student.adelaide.edu.au>
2025-06-20 14:00:07 +0000

    [automatic commit] 2025-06-20 14:00:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -154,7 +154,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
         SELECT WR.request_id, D.name AS dog_name, WR.requested_time, WR.duration_minutes, WR.location, U.username AS owner_username
         FROM WalkRequests WR
         JOIN Dogs D ON WR.dog_id = D.dog_id
-        JOIN Users U ON D.owner_id 
+        JOIN Users U ON D.owner_id = U.user_id
+        WHERE WR.status = 'open'
         `);
     res.json(rows);
   } catch (err) {
