e3e376e73ded46054dd9734b99bbecf84bd27f52
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:03:27 +0000

    [automatic commit] 2025-06-20 13:03:27 UTC

--- part1/app.js
+++ part1/app.js
@@ -105,7 +105,8 @@ let db;
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('barto123', 'barto@example.com', 'hashedbarto', 'owner')
       `);
     }
   } catch (err) {
