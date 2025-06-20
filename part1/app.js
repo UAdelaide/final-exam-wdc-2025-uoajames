316cff6d4c3ed21b987922fa15f6dfb4feabdd01
james <james.lee@student.adelaide.edu.au>
2025-06-20 11:00:34 +0000

    [automatic commit] 2025-06-20 11:00:34 UTC

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database
