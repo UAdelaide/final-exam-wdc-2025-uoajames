25184cb232c90b21dd471eca73174d242f86bae4
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:57:25 +0000

    [automatic commit] 2025-06-20 13:57:25 UTC

--- part1/app.js
+++ part1/app.js
@@ -151,7 +151,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await db.execute(`
-        SELECT WR.
+        SELECT WR.request_id, D.
         `);
     res.json(rows);
   } catch (err) {
