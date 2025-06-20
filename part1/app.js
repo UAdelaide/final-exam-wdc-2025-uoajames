71619c3bb608971990bad6fa1dbe222df93eb7ba
james <james.lee@student.adelaide.edu.au>
2025-06-20 12:57:42 +0000

    [automatic commit] 2025-06-20 12:57:42 UTC

--- part1/app.js
+++ part1/app.js
@@ -56,7 +56,15 @@ let db;
     )
     `);
 
-    
+    await db.execute(`
+      CREATE TABLE Dogs (
+        dog_id INT AUTO_INCREMENT PRIMARY KEY,
+        owner_id INT NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        size ENUM('small', 'medium', 'large') NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+    )
+    `);
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
