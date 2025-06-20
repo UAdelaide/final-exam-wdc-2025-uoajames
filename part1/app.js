f59493c5280bbf847abdf8b4fc2e5731cfd1b43f
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:03:48 +0000

    [automatic commit] 2025-06-20 13:03:48 UTC

--- part1/app.js
+++ part1/app.js
@@ -108,6 +108,15 @@ let db;
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
         ('barto123', 'barto@example.com', 'hashedbarto', 'owner')
       `);
+
+      await db.query(`
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('barto123', 'barto@example.com', 'hashedbarto', 'owner')
+      `);
+
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
