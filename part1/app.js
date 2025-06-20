b7d05b4d9f30966000fcaf5aec0cfd4d49c344a2
james <james.lee@student.adelaide.edu.au>
2025-06-20 14:01:49 +0000

    [automatic commit] 2025-06-20 14:01:49 UTC

--- part1/app.js
+++ part1/app.js
@@ -163,15 +163,11 @@ app.get('/api/walkrequests/open', async (req, res) => {
   }
 });
 
-// Route to return open walk requests as JSON
-app.get('/api/walkrequests/open', async (req, res) => {
+// Route to return walkers summary as JSON
+app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [rows] = await db.execute(`
-        SELECT WR.request_id, D.name AS dog_name, WR.requested_time, WR.duration_minutes, WR.location, U.username AS owner_username
-        FROM WalkRequests WR
-        JOIN Dogs D ON WR.dog_id = D.dog_id
-        JOIN Users U ON D.owner_id = U.user_id
-        WHERE WR.status = 'open'
+        SELECT 
         `);
     res.json(rows);
   } catch (err) {
