7e417a61b399150cd769a79c9d3f2b7d3419e5e0
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:04:08 +0000

    [automatic commit] 2025-06-20 13:04:08 UTC

--- part1/app.js
+++ part1/app.js
@@ -110,7 +110,7 @@ let db;
       `);
 
       await db.query(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
+        INSERT INTO Dogs (name, size, owner_id) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
