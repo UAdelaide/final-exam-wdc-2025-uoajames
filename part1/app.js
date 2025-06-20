948f704fa81105e14bdd6d3b2cf0f3ae538e50d5
james <james.lee@student.adelaide.edu.au>
2025-06-20 14:19:01 +0000

    [automatic commit] 2025-06-20 14:19:01 UTC

--- part1/app.js
+++ part1/app.js
@@ -174,8 +174,8 @@ app.get('/api/walkers/summary', async (req, res) => {
           SELECT COUNT(*)
           FROM WalkRatings R
           JOIN WalkRequests WR ON R.request_id = WR.request_id
-          WHERE R.status = 
-        )
+          WHERE R.status = 'completed'
+        ) AS completed_walks
         `);
     res.json(rows);
   } catch (err) {
