bed71e19deed52387d1afbf319c1e7012a2d1685
james <james.lee@student.adelaide.edu.au>
2025-06-20 11:14:06 +0000

    [automatic commit] 2025-06-20 11:14:06 UTC

--- part1/app.js
+++ part1/app.js
@@ -37,13 +37,13 @@ let db;
     // Create a table if it doesn't exist
     await db.execute(`
       CREATE TABLE Users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            role ENUM('owner', 'walker') NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
     `);
 
     // Insert data if table is empty
