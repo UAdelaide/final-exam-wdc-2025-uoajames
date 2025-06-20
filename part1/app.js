1ba32bbbe343d1e46b0f0631b8bc4002f7bb29a1
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:08:52 +0000

    [automatic commit] 2025-06-20 13:08:52 UTC

--- part1/app.js
+++ part1/app.js
@@ -134,10 +134,10 @@ let db;
 })();
 
 // Route to return books as JSON
-app.get('/', async (req, res) => {
+app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
+    const [rows] = await db.execute('SELECT * FROM books');
+    res.json(rows);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
   }
