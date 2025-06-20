67fa6a7360e49b3373f26fd0c6110c30d9825e1f
james <james.lee@student.adelaide.edu.au>
2025-06-20 14:48:44 +0000

    [automatic commit] 2025-06-20 14:48:44 UTC

--- part1/app.js
+++ part1/app.js
@@ -131,7 +131,6 @@ let db;
       await db.query(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
     ((SELECT request_id FROM WalkRequests WHERE requested_time = '2025-06-01 08:00:00'), (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123'), 5, 'Amazing!'),
-    ((SELECT request_id FROM WalkRequests WHERE requested_time = '2025-06-02 09:00:00'), (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123'), 4, 'Satisfied'),
     ((SELECT request_id FROM WalkRequests WHERE requested_time = '2025-06-04 16:00:00'), (SELECT user_id FROM Users WHERE username = 'barto123'), (SELECT user_id FROM Users WHERE username = 'alice123'), 3, 'Alright')
       `);
 
