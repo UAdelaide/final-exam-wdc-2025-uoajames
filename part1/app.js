46afef966d4d860582b833f5541562825510e74f
james <james.lee@student.adelaide.edu.au>
2025-06-20 13:01:05 +0000

    [automatic commit] 2025-06-20 13:01:05 UTC

--- part1/app.js
+++ part1/app.js
@@ -101,7 +101,7 @@ let db;
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
-      await db.execute(`
+      await db.query(`
         INSERT INTO books (title, author) VALUES
         ('1984', 'George Orwell'),
         ('To Kill a Mockingbird', 'Harper Lee'),
