a54d3c80ec30c0b76d9e83128b156a25c0f3f7b9
james <james.lee@student.adelaide.edu.au>
2025-06-21 07:07:47 +0000

    [automatic commit] 2025-06-21 07:07:47 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+// Route to return dogs as JSON
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.execute(`
+        SELECT D.name AS dog_name, D.size, U.username AS owner_username
+        FROM Dogs D
+        JOIN Users U ON D.owner_id = U.user_id
+        `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
