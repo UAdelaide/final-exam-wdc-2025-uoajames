28fefd1c5f57ec27c38142cb28519514377222b1
james <james.lee@student.adelaide.edu.au>
2025-06-21 04:03:27 +0000

    [automatic commit] 2025-06-21 04:03:27 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,8 +68,9 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      return res.status(500).json({ error: })
+      return res.status(500).json({ error: 'Logout failed' });
     }
+    res.clearCookie('connect.sid');
   })
 }
 
