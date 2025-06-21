6936e0fc8035187ee5f135fa32b6e45870df5b78
james <james.lee@student.adelaide.edu.au>
2025-06-21 06:51:39 +0000

    [automatic commit] 2025-06-21 06:51:39 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const dogs = require('./models');
+const db = require
 
 router.get('/dogs', async (req, res) => {
     try {
-        const dogs = await Dog.find();
+        const [rows] = await d
         res.json(dogs);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch dog image' });
