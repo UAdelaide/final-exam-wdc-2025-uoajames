ce5624e94ab198eaaf2c31541c0492340a2cac39
james <james.lee@student.adelaide.edu.au>
2025-06-21 06:25:58 +0000

    [automatic commit] 2025-06-21 06:25:58 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { Dog } = require('./models');
 
-router.get('/dogs', async (req, res ))
+router.get('/dogs', async (req, res) => {
+    try {
+        const dogs = 
+    }
+})
 
 module.exports = router;
