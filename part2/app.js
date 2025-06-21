7ae2bf380e5f898931889413256f57fd2b8ff375
james <james.lee@student.adelaide.edu.au>
2025-06-21 04:45:00 +0000

    [automatic commit] 2025-06-21 04:45:00 UTC

--- part2/app.js
+++ part2/app.js
@@ -25,7 +25,7 @@ const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs',
+app.use('/api/dogs', dogRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
