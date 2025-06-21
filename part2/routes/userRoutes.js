0acb5d0f2269fe817e675a3575b3921bbe700357
james <james.lee@student.adelaide.edu.au>
2025-06-21 04:43:01 +0000

    [automatic commit] 2025-06-21 04:43:01 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,8 @@ router.get('/dogs', async (req, res) => {
     const[rows] = await db.query(`
       SELECT dog_id, name FROM Dogs WHERE owner_id = ?'`,
       [user.user_id]
-  )
+  );
+  res.json(rows);
   }
 })
 
