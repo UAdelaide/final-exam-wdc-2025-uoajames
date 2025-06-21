0c7a640358ecd9ac69d33d373011005ba1550254
james <james.lee@student.adelaide.edu.au>
2025-06-21 03:40:24 +0000

    [automatic commit] 2025-06-21 03:40:24 UTC

--- part2/app.js
+++ part2/app.js
@@ -18,34 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Dashboard Routes
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-require('dotenv').config();
 
-const app = express();
-
-app.use(session({
-    secret: 'DogWalkService',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}));
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
-
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
-
-// Export the app instead of listening here
-module.exports = app;
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
