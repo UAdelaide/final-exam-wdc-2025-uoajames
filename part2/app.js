5186c4d8a635aa3b93ac2f75651fe54cfadabcac
james <james.lee@student.adelaide.edu.au>
2025-06-21 04:47:58 +0000

    [automatic commit] 2025-06-21 04:47:58 UTC

--- part2/app.js
+++ part2/app.js
@@ -21,11 +21,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs', dogRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
