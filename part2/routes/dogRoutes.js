d8c5bbcad38466587d85f4bbbd848ec3dd9da026
james <james.lee@student.adelaide.edu.au>
2025-06-21 07:02:50 +0000

    [automatic commit] 2025-06-21 07:02:50 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,7 @@ const db = require('../models/db');
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT D.dog_id, D.name AS dog_name, D.size, U.username AS owner_username
+      SELECT D.dog_id, D.name AS dog_name, D.size, U.user_id AS owner_id
       FROM Dogs D
       JOIN Users U ON D.owner_id = U.user_id
     `);
